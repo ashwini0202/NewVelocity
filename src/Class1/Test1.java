81858487318de1ba8ce5b4a043a05f02c91aecdf
Ashwini Biradar <ashwinichambole@gmail.com>
2024-09-26 19:33:31 +0200

    Added new sop statement

--- src/Class1/Test1.java
+++ src/Class1/Test1.java
@@ -1,7 +1,12 @@
 package Class1;
 
 public class Test1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("First gitlab");
+        System.out.println("First gitlab");
+        System.out.println("second gitlab");
         System.out.println("Hello java");
     }
+
 }
